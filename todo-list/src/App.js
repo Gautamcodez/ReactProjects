5be19c2cad2d00fe97b9fdaaeecfaec8880eefbe
Gautam Sharma <Gautamcodez@gmail.com>
2022-09-25 11:27:32 +0530

    completed the ui for the Todo list app

---
+++ todo-list/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='main_div'>
+      <div className='center_div'>
+      <br/>
+      <h1>ToDO List</h1>
+      <br />
+      <input type= 'text' placeholder='Add items' />
+      <button>+</button>
+
+      <ol>
+        <li>Buy a apple</li>
+      </ol>
+
+      </div>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
